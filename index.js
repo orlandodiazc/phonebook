9e3d7fc2cbf43f7c469ebac93dfc3c1d712d2e30
Orlando Diaz <orlandodiazconde@gmail.com>
2023-04-11 11:11:01 -0500

    Update package scripts

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const app = express()
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms :body'))
+app.use(cors())
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!(body.name && body.number)) {
+    return res.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const doesNameExist = persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())
+  console.log(doesNameExist)
+  if (doesNameExist) {
+    return res.status(409).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.random()*10000,
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).json({error: 'the id does not exist'})
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter((person) => person.id !== id)
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  const currentDate = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}}</p>`)
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
